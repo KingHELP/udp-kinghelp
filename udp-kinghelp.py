9a71aba065166f7747e16aae400fc2cd5ca5f7af
KingHELP <129971765+KingHELP@users.noreply.github.com>
2024-02-20 11:30:35 +0000

    Update udp-kinghelp.py

--- udp-kinghelp.py
+++ udp-kinghelp.py
@@ -1,13 +1,12 @@
-# author:
-# █ ▄▀█ █▀▄▀█ ▀█▀ █▀█ █▀█ █▀ ▀█▀
-# █ █▀█ █░▀░█ ░█░ █▄█ ▀▀█ ▄█ ░█░
-# name:
+   main()
+#author:
+#█ ▄▀█ █▀▄▀█ ▀█▀ █▀█ █▀█ █▀ ▀█▀
+#█ █▀█ █░▀░█ ░█░ █▄█ ▀▀█ ▄█ ░█░
+#name:
 # UDP-▀█▀ █▀█ █▀█ █▀ ▀█▀ █▀▀ █▀█
 #    ░█░ █▄█ ▀▀█ ▄█ ░█░ ██▄ █▀▄
-# description:
-# A tool lets you send udp flood on servers with internet traffic to prevent users from accessing connected online services or minecraft servers.
-# warning: 
-# This is strongly for educational purposes only and for testing your ddos protection, I am not responsible for any damages you have or may cause.
+#description:
+# A tool lets you send udp flood on a serverx with internet traffic to prevent users from accessing connected online services and minecraft servers
 
 import os
 import random
